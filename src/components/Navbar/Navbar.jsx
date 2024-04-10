0ee181ae08b2f16daa347b6a474e79702654a279
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2024-04-10 11:31:56 -0100

    feat: responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import "./Navbar.css";
 
+function slideTo(componentID) {
+  document.getElementById(componentID)?.scrollIntoView({ behavior: "smooth" });
+}
 const Navbar = () => {
-  const slideToHome = () => { document.getElementById('home')?.scrollIntoView({ behavior: 'smooth' }) }
-  const slideToServices = () => { document.getElementById('services')?.scrollIntoView({ behavior: 'smooth' }) }
-  const slideToAbout = () => { document.getElementById('About')?.scrollIntoView({ behavior: 'smooth' }) }
-  const slideToFQA = () => { document.getElementById('FQA')?.scrollIntoView({ behavior: 'smooth' }) }
+  const [isMenuOpen, setMenuIsOpen] = useState(false);
+
   return (
     <nav>
       <div className="logo-container">
         <h3>Safe</h3>
       </div>
-      <div className="nav-links">
-        <button className="button-link"  onClick={slideToHome}>
+      <button
+        className="toggle-menu"
+        onClick={() => setMenuIsOpen(!isMenuOpen)}
+      >
+        <span className={!isMenuOpen ? "bar1" : "bar1-close"}></span>
+        <span className={!isMenuOpen ? "bar2" : "bar2-close"}></span>
+        <span className={!isMenuOpen ? "bar3" : "bar3-close"}></span>
+
+      </button>
+      <div className={isMenuOpen ? "nav-links" : "nav-links nav-hidden"}>
+        <button className="button-link" onClick={slideTo("home")}>
           Home
         </button>
-        <button className="button-link" onClick={slideToServices}>
+        <button className="button-link" onClick={slideTo("services")}>
           Serviços
         </button>
-        <button className="button-link" onClick={slideToAbout}>
+        <button className="button-link" onClick={slideTo("about")}>
           Sobre
         </button>
-        <button className="button-link" onClick={slideToFQA}>
+        <button className="button-link" onClick={slideTo("fqa")}>
           FQA
         </button>
       </div>
