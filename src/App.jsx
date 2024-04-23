0076174cda3a25f734a72d319aba8454aa48309a
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2024-04-23 22:28:17 -0100

    feat: sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import "./App.css"
-import Landing from "./sections/Landing/Landing"
+import "./App.css";
+import CallToAction from "./sections/CallToAction/CallToAction";
+import Comments from "./sections/Comments/Comments";
+import FAQ from "./sections/FAQ/FAQ";
+import Landing from "./sections/Landing/Landing";
+import Services from "./sections/Services/Services";
 function App() {
   return (
     <>
-   <Landing/>
+      <Landing />
+      <Services />
+      <CallToAction />
+      <Comments />
+      <FAQ />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
