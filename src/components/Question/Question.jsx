422daad0453c764782e9f9183d4b0828a72e9710
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2024-04-25 21:22:08 -0100

    fix: FAQ on open animation

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -10,13 +10,13 @@ const Question = () => {
     }
 
   return (
-    <div className="question">
-        <div className={isOpen?"question-title-open":"question-title"}>
-            <p>Essa é uma pergunta sobre alguma coisa</p>
+    <div className="question-container">
+        <div className="question-title">
+            <p className="question">Essa é uma pergunta sobre alguma coisa</p>
             <img className={isOpen?"arrow-open":"arrow"} src={Arrow} alt="arrow" onClick={toggleOpen}/>
         </div>
         <div className={isOpen?"question-response": "question-response-hidden"}>
-            <p>Isso é a resposta para alguma questão que ja foi feita Isso é a resposta para alguma questão que ja foi feitaIsso é a resposta para alguma questão que ja foi feitaIsso é a resposta para alguma questão que ja foi feitaIsso é a resposta para alguma questão que ja foi feita</p>
+            <p className="question-response-text">Isso é a resposta para alguma questão que ja foi feita Isso é a resposta para alguma questão que ja foi feitaIsso é a resposta para alguma questão que ja foi feitaIsso é a resposta para alguma questão que ja foi feitaIsso é a resposta para alguma questão que ja foi feita</p>
         </div>
         
     </div>
