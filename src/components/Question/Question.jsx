0829f17ea58b617ccca89869c6e2cca0919d3dbd
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2024-04-25 21:37:29 -0100

    fix: question container cursor pointer and click

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -10,10 +10,10 @@ const Question = () => {
     }
 
   return (
-    <div className="question-container">
+    <div className="question-container" onClick={toggleOpen}>
         <div className="question-title">
             <p className="question">Essa é uma pergunta sobre alguma coisa</p>
-            <img className={isOpen?"arrow-open":"arrow"} src={Arrow} alt="arrow" onClick={toggleOpen}/>
+            <img className={isOpen?"arrow-open":"arrow"} src={Arrow} alt="arrow"/>
         </div>
         <div className={isOpen?"question-response": "question-response-hidden"}>
             <p className="question-response-text">Isso é a resposta para alguma questão que ja foi feita Isso é a resposta para alguma questão que ja foi feitaIsso é a resposta para alguma questão que ja foi feitaIsso é a resposta para alguma questão que ja foi feitaIsso é a resposta para alguma questão que ja foi feita</p>
