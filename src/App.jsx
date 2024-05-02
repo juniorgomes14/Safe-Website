1ab4c8347ed2d959aba60d30b0e454f7c17b8581
NunoLima10 <nunolima711@gmail.com>
2024-05-02 20:22:54 -0100

    fix: scroll up button not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import About from "./sections/About/About";
 import CallToAction from "./sections/CallToAction/CallToAction";
 import Comments from "./sections/Comments/Comments";
 import FAQ from "./sections/FAQ/FAQ";
 import Landing from "./sections/Landing/Landing";
 import Services from "./sections/Services/Services";
 import Footer from "./sections/Footer/Footer"
+import ScrollUpButton from "./components/ScrollUpButton/ScrollUpButton"
 function App() {
   return (
     <>
@@ -13,9 +13,11 @@ function App() {
       <Services />
       <CallToAction />
       <Comments />
-      <About />
+      {/* <About /> */}
       <FAQ />
       <Footer/>
+      <ScrollUpButton/>
+      
     </>
   );
 }
