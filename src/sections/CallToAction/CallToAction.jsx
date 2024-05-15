7af23ff8c1944d567768b6d572f83a9cf2f6d82d
NunoLima10 <nunolima711@gmail.com>
2024-05-15 13:43:16 -0100

    fix: call to action text

--- src/sections/CallToAction/CallToAction.jsx
+++ src/sections/CallToAction/CallToAction.jsx
@@ -4,9 +4,9 @@ import "./CallToAction.css"
 const CallToAction = () => {
   return (
     <section className="call-to-action">
-        <h2 className="action-title">Proteja seu bens em segurança agora </h2>
+        <h2 className="action-title">Faça a escolha certa para proteger os seus bens</h2>
         <div className="call-to-action-container"></div>
-        <p className="action-description">Lorem ipsum dolor sit amet consectetur. Nulla adipiscing cras in felis viverra id congue odio id. </p>
+        <p className="action-description">Não perca mais tempo! Peça o seu orçamento sem compromisso agora mesmo e conte com profissionais qualificados.</p>
         <button className="cta-button-white"><a href="https://wa.me/2389725617" className="link" target="blank">Contactar Agora</a></button>
       <img src={BackGroundImage} alt='back-ground-image'/>
     </section>
